e9c5d94a7dfc26df2701588af090d68b956d62b5
Gloria Edith Inga Llicahua <gingalli@emeal.nttdata.com>
2022-10-16 17:08:48 -0500

    arrow functions

--- ES6/arrowFunctions.js
+++ ES6/arrowFunctions.js
@@ -2,6 +2,8 @@ function printMessage(){
     console.log("Message");
 }
 
+const suma = (a, b) => a + b;
+
 const printMessage = () => {
     console.log("Message");
 }
@@ -9,3 +11,40 @@ const printMessage = () => {
 () => {
     console.log("Message");
 }
+
+/** THIS **/
+function product(){
+   this.name = "iPad";
+   this.price = 100; 
+   this.printInfo = function(){
+      setTimeout(function(){
+           console.log(this);
+       }, 5000);       
+   }
+}
+/** new = Object.create() **/
+var product = new product();
+product.printInfo();
+
+function hello(){ 
+   console.log(this);
+}
+
+hello();
+
+/** THIS **/
+function product(){
+   this.name = "iPad";
+   this.price = 100; 
+   this.printInfo = function(){
+      setTimeout(() => {
+           console.log(this);
+       }, 1000);       
+   }
+}
+/** new = Object.create() **/
+var product = new product();
+product.printInfo();
+
+
+
