94ce5da27ba8e17b7dbf7b5c3a23ca5594df1c32
Gloria Edith Inga Llicahua <gingalli@emeal.nttdata.com>
2022-10-16 17:59:22 -0500

    Destructuring

---
+++ ES6/destructuring.js
@@ -0,0 +1,12 @@
+var arrayCountries = ["Peru", "Chile", "Colombia", "Argentina", "Brasil"];
+const [peru, chile, colombia] = arrayCountries;
+console.log(peru, chile, colombia);
+
+const user = {
+    username: "dev01",
+    role: "admin",
+    fullname: "Juan Cortez"
+};
+
+const {username, role, fullname} = user;
+console.log(username, role, fullname);
