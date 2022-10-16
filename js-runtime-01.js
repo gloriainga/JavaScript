ccc58e5de27d3eaa180d1fb92dd03ec4efdf101b
Gloria Edith Inga Llicahua <gingalli@emeal.nttdata.com>
2022-10-15 21:51:46 -0500

    Javascript Runtime

--- js-runtime-01.js
+++ js-runtime-01.js
@@ -2,6 +2,12 @@ function printMessage(msg){
     setTimeout(function(){
         console.log("SET TIMEOUT 5 SEGUNDOS");
     }, 5000);
+    setTimeout(function(){
+        console.log("SET TIMEOUT 1 SEGUNDOS");
+    }, 1000);
+    setTimeout(function(){
+        console.log("SET TIMEOUT 0 SEGUNDOS");
+    }, 0);
     console.log("Inicio de funcion");
     console.log("Mensaje: " + msg);
     console.log("Fin de funcion");
